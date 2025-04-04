d852ae60151a3a0b7acfb9118cc32ff096b314a7
Jhonata Lopes <jhonatalopes.desenvolvedor@gmail.com>
2025-04-04 13:17:55 -0300

    Adiciona script para instalação e configuração do MySQL: - Gera uma senha aleatória para o usuário root do MySQL - Instala o MySQL Server de forma não interativa - Configura o acesso remoto para o usuário root

---
+++ mysql.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+clear
+
+setenforce 0 >> /dev/null 2>&1
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Gera uma senha aleatória
+MYSQL_ROOT_PASSWORD=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 16)
+echo "Senha gerada para o MySQL root: $MYSQL_ROOT_PASSWORD"
+
+# Configura a senha do root do MySQL
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+
+# Instala o MySQL Server
+apt-get install -y mysql-server
+
+# Configura o MySQL
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP USER 'root'@'localhost'; FLUSH PRIVILEGES;"
+
+echo $MYSQL_ROOT_PASSWORD
